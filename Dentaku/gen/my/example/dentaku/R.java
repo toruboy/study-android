cb16d712e252b357f84ff931d73020df3d4fd3c5
Toru Watanabe <toru.watanabe0329@gmail.com>
2014-03-08 22:15:43 +0900

    デザイン変更

--- Dentaku/gen/my/example/dentaku/R.java
+++ Dentaku/gen/my/example/dentaku/R.java
@@ -10,6 +10,20 @@ package my.example.dentaku;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black_center=0x7f040007;
+        public static final int black_end=0x7f040008;
+        public static final int black_start=0x7f040006;
+        public static final int gray_center=0x7f040004;
+        public static final int gray_end=0x7f040005;
+        public static final int gray_start=0x7f040003;
+        public static final int push_center=0x7f04000a;
+        public static final int push_end=0x7f04000b;
+        public static final int push_start=0x7f040009;
+        public static final int red_center=0x7f040001;
+        public static final int red_end=0x7f040002;
+        public static final int red_start=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,53 +31,56 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_black=0x7f020000;
+        public static final int button_gray=0x7f020001;
+        public static final int button_red=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001c;
-        public static final int allClear=0x7f080011;
-        public static final int clear=0x7f08000b;
-        public static final int comma=0x7f080019;
-        public static final int display=0x7f080001;
-        public static final int divide=0x7f080009;
-        public static final int doublezero=0x7f080018;
-        public static final int eight=0x7f080007;
-        public static final int equal=0x7f08001a;
-        public static final int five=0x7f08000d;
-        public static final int four=0x7f08000c;
-        public static final int linearLayout1=0x7f080000;
-        public static final int linearLayout2=0x7f080002;
-        public static final int minus=0x7f080015;
-        public static final int multiply=0x7f08000f;
-        public static final int nine=0x7f080008;
-        public static final int one=0x7f080012;
-        public static final int plus=0x7f08001b;
-        public static final int seven=0x7f080006;
-        public static final int sign=0x7f080005;
-        public static final int six=0x7f08000e;
-        public static final int tableLayout1=0x7f080003;
-        public static final int tableRow1=0x7f080004;
-        public static final int tableRow2=0x7f08000a;
-        public static final int tableRow3=0x7f080010;
-        public static final int tableRow4=0x7f080016;
-        public static final int three=0x7f080014;
-        public static final int two=0x7f080013;
-        public static final int zero=0x7f080017;
+        public static final int action_settings=0x7f09001c;
+        public static final int allClear=0x7f090011;
+        public static final int clear=0x7f09000b;
+        public static final int comma=0x7f090019;
+        public static final int display=0x7f090001;
+        public static final int divide=0x7f090009;
+        public static final int doublezero=0x7f090018;
+        public static final int eight=0x7f090007;
+        public static final int equal=0x7f09001a;
+        public static final int five=0x7f09000d;
+        public static final int four=0x7f09000c;
+        public static final int linearLayout1=0x7f090000;
+        public static final int linearLayout2=0x7f090002;
+        public static final int minus=0x7f090015;
+        public static final int multiply=0x7f09000f;
+        public static final int nine=0x7f090008;
+        public static final int one=0x7f090012;
+        public static final int plus=0x7f09001b;
+        public static final int seven=0x7f090006;
+        public static final int sign=0x7f090005;
+        public static final int six=0x7f09000e;
+        public static final int tableLayout1=0x7f090003;
+        public static final int tableRow1=0x7f090004;
+        public static final int tableRow2=0x7f09000a;
+        public static final int tableRow3=0x7f090010;
+        public static final int tableRow4=0x7f090016;
+        public static final int three=0x7f090014;
+        public static final int two=0x7f090013;
+        public static final int zero=0x7f090017;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -87,10 +104,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
